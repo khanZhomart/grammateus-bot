25edcf07ab970e88817efe724749176b836d3ebb
jtalgatulu@yandex.kz <jtalgatulu@yandex.kz>
2022-02-14 17:14:43 +0600

    Установил локальную базу данных

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { Scenes, session, Telegraf } from "telegraf"
 import dotenv from 'dotenv'
 import scenes from "./src/scenes/scenes.js"
 import { sendLogs } from "./src/services/session.service.js"
+import { pushChat, getChat, resetDb } from "./src/database/db.js"
 
 dotenv.config()
 
@@ -23,10 +24,24 @@ bot.command('quiz', (ctx) => {
     if (ctx.message.chat.type === 'private')
         return
 
-    return ctx.scene.enter('GROUP_MENU_SCENE')
+    pushChat(ctx.message.chat.id, (quiz_started) => {
+        if (quiz_started) {
+            return
+        }
+
+        return ctx.scene.enter('GROUP_MENU_SCENE')  
+    })
 })
 
-bot.command('cmd', (ctx) => sendLogs(ctx))
+bot.command('/fuck_db', (ctx) => resetDb())
+
+bot.command('cmd', (ctx) => {
+    getChat(ctx.message.chat.id, (data) => {
+        return ctx.reply(`<pre>${JSON.stringify(data, null, 2)}</pre>`, {
+            parse_mode: 'HTML'
+        })
+    })
+})
 
 bot.on('poll', (ctx) => console.log(ctx.poll))
 
